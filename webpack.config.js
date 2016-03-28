4843dc979cb9cb4f3c0f160eb9df3e0fdac8f51d
David Raynes <David.Raynes@teamaol.com>
2016-03-28 17:10:15 -0400

    Add resolve.root to base weback.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+var path = require('path');
+
 module.exports = {
   cache: true,
   entry: './js/index',
   output: {
     path: __dirname + '/public',
     filename: 'browser-bundle.js'
+  },
+  resolve: {
+    root: [
+      path.resolve('./js')
+    ]
   },
   module: {
     loaders: []
