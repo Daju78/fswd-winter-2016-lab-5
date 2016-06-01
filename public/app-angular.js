418bdf44677966345e37f0711ca51d40b78fe3f8
David Raynes <rayners@gmail.com>
2016-06-01 19:12:44 -0400

    Controllers and services!

--- public/app-angular.js
+++ public/app-angular.js
@@ -2,13 +2,25 @@
 var angular = require('angular');
 
 angular.module('fswd.todo', [])
-  .controller('TodoController', function() {
-    var vm = this;
-    vm.todoList = ['Laundry', 'Groceries'];
-
-    vm.addTodo = function(newTodo) {
-      vm.todoList = vm.todoList.concat([newTodo]);
+  .service('TodoService', function() {
+    var service = this;
+    service.todoList = ['Laundry', 'Groceries'];
+    service.addTodo = function(newTodo) {
+      service.todoList = service.todoList.concat([newTodo]);
     };
+  })
+  .controller('TodoController', function(TodoService, $scope) {
+    var vm = this;
+    vm.addTodo = function(newTodo) {
+      TodoService.addTodo(newTodo);
+    };
+
+    $scope.$watch(function() {
+      return TodoService.todoList;
+    }, function(newValue, oldValue) {
+      vm.todoList = newValue;
+    });
+
   });
 
 angular.bootstrap(document, ['fswd.todo']);
