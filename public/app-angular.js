2c4e94bf39d23b388f77728acc902036f0a9205f
David Raynes <rayners@gmail.com>
2016-06-13 17:46:12 -0400

    Starting on angular-side registration validation

--- public/app-angular.js
+++ public/app-angular.js
@@ -1,7 +1,7 @@
 
 var angular = require('angular');
 
-angular.module('fswd.todo', [])
+angular.module('fswd.todo', ['fswd.todo.registration'])
   .service('TodoService', function($http) {
     var service = this;
     service.todoList = ['Laundry', 'Groceries'];
@@ -38,6 +38,37 @@ angular.module('fswd.todo', [])
         });
     }, 5000);
 
+  });
+
+angular.module('fswd.todo.registration', [])
+  .directive('registrationPasswordMatch', function() {
+    return {
+      restrict: 'A',
+      require: ['^ngModel', '^form'],
+      link: function(scope, element, attrs, ctrl) {
+        var passwordController = ctrl[0];
+        var formController = ctrl[1];
+        var confirmPasswordController = formController[attrs.registrationPasswordMatch];
+
+        scope.$watch(function() {
+          return confirmPasswordController.$viewValue + passwordController.$viewValue;
+        }, function() {
+          passwordController.$$parseAndValidate();
+          confirmPasswordController.$$parseAndValidate();
+        });
+
+        passwordController.$validators.passwordMatch = function(modelValue) {
+          return (!modelValue && !confirmPasswordController.$modelValue) || (modelValue === confirmPasswordController.$modelValue);
+        };
+
+        confirmPasswordController.$validators.passwordMatch = function(modelValue) {
+          return (!modelValue && !passwordController.$viewValue) || (modelValue === passwordController.$viewValue);
+        };
+      }
+    };
+  })
+  .controller('RegistrationFormController', function() {
+    var vm = this;
   });
 
 angular.bootstrap(document, ['fswd.todo']);
